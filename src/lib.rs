0767e7822c21255b3ac4821ba2d70ea42f83b52b
eric.olson <eric.rob.olson@gmail.com>
2025-10-21 10:11:23 -0700

    Began new app

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub struct App {}
+impl App {
+    pub fn new() -> Self {
+        Self {}
+    }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn run(mut self) -> Result<(), Box<dyn std::error::Error>> {
+        Ok(())
     }
 }
