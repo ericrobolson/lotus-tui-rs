40f68592197113795cae828c5f46d6a60c6b7ab4
eric.olson <eric.rob.olson@gmail.com>
2025-10-21 10:18:08 -0700

    Stubbed out basic program loop

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,42 @@
-pub struct App {}
-impl App {
-    pub fn new() -> Self {
-        Self {}
+/// Main application for building TUI programs.
+pub struct App<State> {
+    state: State,
+}
+impl<State> App<State> {
+    /// Create a new application with the given state.
+    pub fn new(state: State) -> Self {
+        Self { state }
     }
 
-    pub fn run(mut self) -> Result<(), Box<dyn std::error::Error>> {
+    /// Run the application.
+    pub fn run(
+        &mut self,
+        mut update: impl FnMut(Context<State>) -> UpdateResult,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        loop {
+            let context = Context {
+                state: &mut self.state,
+            };
+            match update(context) {
+                UpdateResult::Continue => (),
+                UpdateResult::Exit => break,
+            }
+        }
+
         Ok(())
     }
+}
+
+/// Context for building UI screens.
+pub struct Context<'a, State> {
+    pub state: &'a mut State,
+}
+
+/// Result of updating the application.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum UpdateResult {
+    /// Continue to the next update.
+    Continue,
+    /// Exit the application.
+    Exit,
 }
